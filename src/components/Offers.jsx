7b7e106eeab9af9adf4524591313bfa08bdf4879
Gabriel Matos <matos7771@hotmail.com>
2024-11-22 20:32:58 -0300

    Import swiper Offers page

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -1,15 +1,16 @@
 import styled from "styled-components";
+import '../index.css';
 
 const OffersContainer = styled.section`
     width: 1440px;
     height: 875px;
-    background-color: red;
+  background-color: var(--light-gray-3);
 `
 
 const Offers = () => {
     return ( 
         <OffersContainer className="flex justify-content-center m-auto">
-            <h1>Offer</h1>
+            
         </OffersContainer>
      );
 }
